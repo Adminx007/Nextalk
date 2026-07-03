057dc4922427c9d953a6180d47906e14d98fdaee
Adminx007 <minertopfollow@gmail.com>
2026-07-03 13:49:53 +0600

    Add user authentication and display users
    Implement user authentication and load users from Firestore.

--- js/home.js
+++ js/home.js
@@ -1,1 +1,66 @@
+import { auth, db } from "./firebase.js";
 
+import {
+collection,
+getDocs
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+import {
+onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const usersDiv = document.getElementById("users");
+
+onAuthStateChanged(auth, async(user)=>{
+
+if(!user){
+
+location.href="login.html";
+return;
+
+}
+
+loadUsers(user.uid);
+
+});
+
+async function loadUsers(myUid){
+
+usersDiv.innerHTML="Loading...";
+
+const snap = await getDocs(collection(db,"users"));
+
+usersDiv.innerHTML="";
+
+snap.forEach(doc=>{
+
+const u=doc.data();
+
+if(u.uid===myUid) return;
+
+usersDiv.innerHTML +=`
+
+<div class="user"
+onclick="location.href='chat.html?uid=${u.uid}'">
+
+<div>
+
+<div class="name">
+${u.name}
+</div>
+
+<div class="online">
+${u.online ? "🟢 Online":"⚫ Offline"}
+</div>
+
+</div>
+
+</div>
+
+`;
+
+});
+
+}
